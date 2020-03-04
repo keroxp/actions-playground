7c7cbbb5db0fef6db6bf170f5d6e18943c8f8c5c
keroxp <kerokerokerop@gmail.com>
2020-03-04 20:37:23 +0900

    del

--- hoge.sh
+++
@@ -1,5 +0,0 @@
-          CURR=$(git tag | grep "^[0-9]\+\.[0-9]\+\.[0-9]\+$" | sort -V | tail -n 2 | head -n 1)
-          BODY=$(git log ${NEXT}...${CURR} --oneline)
-          echo "::set-output name=body::${BODY}"
-
-          
